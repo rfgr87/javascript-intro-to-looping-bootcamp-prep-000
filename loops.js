ede1ceda85a5bbe897428d817d67f5df9ddf630e
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-02-06 18:33:06 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -2,10 +2,10 @@
 function forLoop(array){
   for(let i =0; i<25; i++){
     if (i === 1){
-      array = [...array, "I am 1 strange loop"]
+      array.push( "I am 1 strange loop")
     }
     else if(i>1){
-      array = [...array, `I am ${i} strange loops`]
+      array.push(`I am ${i} strange loops`)
     }
   }
   return array
